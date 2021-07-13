4ea699f1568c9877bcc08f2c896ce8e339e7434b
KuzminaYuliya <kuzmina_h@mail.ru>
2021-07-13 23:40:27 +0500

    first commit for ajs_11_3_generator*

---
+++ README.md
@@ -0,0 +1,2 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/b72yoau1c840ywpc?svg=true)](https://ci.appveyor.com/project/KuzminaYuliya/ajs-11-3-generator2)
+
---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+export default function canIterate(obj) {
+  return Boolean(obj) && typeof obj[Symbol.iterator] === 'function';
+}
+/* console.log(canIterate(new Map())); // true
+  console.log(canIterate(new Set())); // true
+  console.log(canIterate(null)); // false
+  console.log(canIterate(10)); // false
+  console.log(canIterate("Netology")); // true */
